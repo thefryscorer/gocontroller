b6e02edf7441fc567b9947bcdfcddfc6be5ead90
Daniel Byron <danielbyron51@gmail.com>
2014-08-08 09:55:00 +1000

    Update main.go

--- example/main.go
+++ example/main.go
@@ -39,8 +39,8 @@ func main() {
 		{Left: 20, Top: 60, Key: "Down"},
 		{Left: 10, Top: 40, Key: "Left"},
 		{Left: 30, Top: 40, Key: "Right"},
-		{Left: 60, Top: 40, Key: "a"},
-		{Left: 80, Top: 40, Key: "b"},
+		{Left: 60, Top: 40, Key: "a", Color:"#204387"},
+		{Left: 80, Top: 40, Key: "b", Color:"#208743"},
 		{Left: 45, Top: 10, Key: "Return"},
 	}}
 	server := gocontroller.NewServer(layout, gocontroller.DefaultPort)
